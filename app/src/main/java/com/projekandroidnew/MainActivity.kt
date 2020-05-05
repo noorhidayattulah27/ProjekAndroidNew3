e5fde5eae926a616a3d3cad77dd43cf97a670e56
noorhidayattulah27 <tajelipuk@gmail.com>
2020-05-06 02:46:07 +0800

    Initial commit

---
+++ app/src/main/java/com/projekandroidnew/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.projekandroidnew
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
